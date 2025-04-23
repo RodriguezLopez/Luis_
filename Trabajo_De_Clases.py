0c15e7f641bed476474fa82f82a26e811cbd2f74
MatiusDev <mateomonsalve502@gmail.com>
2025-04-22 20:58:02 -0500

    Luis

---
+++ Trabajo_De_Clases.py
@@ -0,0 +1,92 @@
+
+
+
+
+
+#CUARTO EJECICIO
+#PIDE UN NUMERO AL USUARIO POR TERMINAL
+#Y DETERMINA SI ES UN NUMERO PRIMO
+
+#QUINTO EJERICIO
+#PIDE POR TERMINAL QUE INGRESEN UN NUMERO ENTERO,
+#SINO ES ENTERO, ENTONCES IMPRIME UN MENSAJE DE ERROR DICIENDO QUE NO SE ACEPTAN DECIMALES
+#ADICIONAL, SI EL NUMERO ES MULTIPLO DE 3, IMPRIME LA PALABRA "FIZZ"
+#SI EL NUMERO ES MULTIPLO DE 5, IMPRIME LA PALABRA "BUZZ"
+#Y SI ES MULTIPLO DE 15, IMPRIME LA PALABRA "FIZZBUZZ"
+
+#PRIMER EJERCICIO:
+#VAN A SOLICITAR LA EDAD DE 2 PERSONAS
+#VAN A GUARDAR LAS EDAD EN VARIABLES
+#VAN A VALIDAR QUIEN ES MAYOR QUE EL OTRO
+#ADICIONAL, QUIEN ES MAYOR DE EDAD
+
+# Solucion ejercicio numero uno
+
+#steban = 17
+#Luis = 22
+
+#if Luis >= Esteban:
+    #print("Luis es mayor de edad")
+#elif Esteban <= Luis:
+    #print("Esteban es menor de edad")
+
+
+
+#SEGUNDO EJERCICIO:
+#SOLICITA EL NOMBRE, EDAD Y NOTAS DE UNIVERSIDAD DE UNA PERSONA
+#VAS A PEDIR UN TOTAL DE 5 NOTAS
+#VAS A CALCULAR EL PROMEDIO DE NOTAS
+#Y SI EL PROMEDIO ES INFERIOR DE 3, ENTONCES IMPRIME POR CONSOLA QUE PERDIO LA MATERIA,
+#SI ES MAYOR O IGUAL A 3, ENTONCES IMPRIME GANASTE
+
+Name = input("Dame tu nombre")
+Ege = input("Cual es tu edad")
+ 
+print("Ingresus sus notas")
+
+Nota1 = float(input("-"))
+Nota2 = float(input("-"))
+Nota3 = float(input("-"))
+Nota4 = float(input("-"))
+Nota5 = float(input("-"))
+
+promemedio = (Nota1+Nota2+Nota3+Nota4+Nota5)/5
+
+if promemedio < 3:
+    print("has perdido la materia")
+elif promemedio >= 3:
+    print("ganaste la materia", promemedio)
+
+
+
+#TERCER EJERCICIO
+#PIDE UN NUMERO AL USUARIO POR TERMINAL
+#Y DETERMINA SI ES UN NUMERO PAR O IMPAR
+
+# Solucion tercer ejercicio 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
